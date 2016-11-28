512d6f5af36033e37cad094d63be02f3b6a340a9
Sebas320 <cbastian320@gmail.com>
2016-11-27 21:45:22 -0500

    Clase Order con constructor y métodos get

--- Buying Online/src/models/entities/Order.java
+++ Buying Online/src/models/entities/Order.java
@@ -1,5 +1,24 @@
 package models.entities;
 
-public class Order {
+import java.util.ArrayList;
 
+public class Order {
+	private int id;
+	private User user;
+	private ArrayList<OrderProduct> products;
+	
+	public Order(int id, User user, ArrayList<OrderProduct> products) {
+		this.id = id;
+		this.user = user;
+		this.products = products;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public ArrayList<OrderProduct> getProducts() {
+		return products;
+	}
+	
 }
