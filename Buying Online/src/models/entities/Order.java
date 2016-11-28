057d811f0e6669a554fb57439c926700318022a3
Sebas320 <cbastian320@gmail.com>
2016-11-27 21:49:42 -0500

    Agregué atributo borrado

--- Buying Online/src/models/entities/Order.java
+++ Buying Online/src/models/entities/Order.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 public class Order {
 	private int id;
 	private User user;
+	private State state;
 	private ArrayList<OrderProduct> products;
 	
-	public Order(int id, User user, ArrayList<OrderProduct> products) {
+	public Order(int id, User user, ArrayList<OrderProduct> products, State state) {
 		this.id = id;
 		this.user = user;
 		this.products = products;
+		this.state = state;
 	}
 
 	public int getId() {
@@ -19,6 +21,10 @@ public class Order {
 
 	public ArrayList<OrderProduct> getProducts() {
 		return products;
+	}
+	
+	public State getState() {
+		return state;
 	}
 	
 }
