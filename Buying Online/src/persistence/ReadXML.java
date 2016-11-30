83f79044e19e4efe1bff310d978b9e92b5972f15
Marco Gonzalez <markiros1324@gmail.com>
2016-11-29 20:44:07 -0500

    Cree paquete persistencia y paquete data, añadi archivo xml para usuarios (OJO el profe dijo usar XML para practicar para el parcial PORFAVOR revisar y repasar)

---
+++ Buying Online/src/persistence/ReadXML.java
@@ -0,0 +1,39 @@
+package persistence;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import models.entities.User;
+
+public class ReadXML {
+
+	public ArrayList<User> read() throws ParserConfigurationException, SAXException, IOException{
+		ArrayList<User> userList = new ArrayList<>();
+		File file = new File("src/data/users.xml");
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		
+		Document document = builder.parse(file);
+		document.getDocumentElement().normalize();
+		
+		NodeList nodeList = document.getElementsByTagName("user");
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			userList.add(new User(((((Element)nodeList.item(i)).getElementsByTagName("name")).item(0)).getTextContent()
+					, ((((Element)nodeList.item(i)).getElementsByTagName("address")).item(0)).getTextContent()
+					, ((((Element)nodeList.item(i)).getElementsByTagName("password")).item(0)).getTextContent()
+					, ((((Element)nodeList.item(i)).getElementsByTagName("sourceImg")).item(0)).getTextContent()
+					));
+		}
+		return userList;
+	}
+}
