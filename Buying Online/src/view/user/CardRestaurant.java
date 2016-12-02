bfc4087e5f2b582a54dd47cf05b59343bba695a9
Marco Gonzalez <markiros1324@gmail.com>
2016-12-02 15:14:43 -0500

    ULTIMO COMENTARIo

--- Buying Online/src/view/user/CardRestaurant.java
+++ Buying Online/src/view/user/CardRestaurant.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.net.URL;
+
 import javax.swing.BorderFactory;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -29,24 +31,33 @@ public class CardRestaurant extends JPanel {
 		GridSystem gridPanel = new GridSystem(this);
 
 		btnImgRestaurant = new JButton(new ImageIcon());
+		btnImgRestaurant.setBorder(null);
 		btnImgRestaurant.addActionListener(controller);
 		btnImgRestaurant.setActionCommand(Actions.SHOW_PRODUCTS_BY_SHOP.toString());
 		add(btnImgRestaurant, gridPanel.insertComponent(1, 1, 5, 0.70));
 
 		lbNameRestaurant = new JLabel();
 		lbNameRestaurant.setFont(new Font("Elephant", Font.ITALIC, 16));
-		add(lbNameRestaurant, gridPanel.insertComponent(1, 6, 5, 0.15));
+		add(lbNameRestaurant, gridPanel.insertComponent(2, 1, 5, 0.15));
+	}
+	
+	public Icon loadImagen(String urlImage) {
+		try {
+			return new ImageIcon(new URL(urlImage));
+		} catch (java.net.MalformedURLException e) {
+			return new ImageIcon("src/img/loadingFail.png");
+		}
 	}
 
-	public Icon reSize(ImageIcon imagen) {
-		Image conversion = imagen.getImage();
+	public Icon reSize(Icon imagen) {
+		Image conversion = ((ImageIcon) imagen).getImage();
 		Image tamanio = conversion.getScaledInstance(300, 70, Image.SCALE_SMOOTH);
 		ImageIcon result = new ImageIcon(tamanio);
 		return result;
 	}
 
 	public void setForm(Shop shop) {
-		btnImgRestaurant.setIcon(reSize(new ImageIcon(shop.getSrcImg())));
+		btnImgRestaurant.setIcon(reSize(loadImagen(shop.getSrcImg())));
 		btnImgRestaurant.setName(String.valueOf(shop.getId()));
 		lbNameRestaurant.setText(shop.getName());
 	}
