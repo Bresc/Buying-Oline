2717b9a93b6fbbddb42fb7648f75bf904c52ee37
diianramirez <DAYAN@Diian>
2016-12-02 14:21:58 -0500

    botones usuario

--- Buying Online/src/view/user/MainWindowUser.java
+++ Buying Online/src/view/user/MainWindowUser.java
@@ -1,10 +1,19 @@
 package view.user;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Image;
 import java.util.ArrayList;
+
+import javax.swing.AbstractButton;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+
+import controller.Actions;
 import controller.Controller;
 import models.entities.Product;
 import models.entities.Shop;
@@ -16,12 +25,16 @@ public class MainWindowUser extends JFrame {
 	
 	private JPanel panelActualCenter;
 	private PanelProductsFromShop panelProductsFromShop;
-	private PanelShop panelShop;
+	private JButton btnFavorites;
+	private JButton btnListProducts;
+	private AbstractButton btnShoppingCar;
+	private JButton btnSettings;
+	private PanelShop PanelShop;
 
 	public MainWindowUser(Controller controller) {
 		setSize(380, 600);
 		setLayout(new BorderLayout());
-//		setUndecorated(true);
+		setUndecorated(true);
 		setLocationRelativeTo(null);
 		setTitle("RestaurantSoft Client v0.01");
 		getContentPane().setBackground(ConstanstUIUser.BACKGROUND_COLOR_MAIN_WINDOW_USER);
@@ -30,20 +43,60 @@ public class MainWindowUser extends JFrame {
 		toolBarUser = new ToolBar(controller);
 		add(toolBarUser, BorderLayout.NORTH);
 
-		panelActualCenter = new JPanel();
-		panelProductsFromShop = new PanelProductsFromShop(controller);
+//		panelActualCenter = new JPanel();
+//		panelProductsFromShop = new PanelProductsFromShop(controller);
+//		
+		PanelShop =  new PanelShop(controller);
+		PanelShop.setBackground(Color.white);
+//		panelActualCenter.add(panelShops);
+//		add(new JScrollPane(panelActualCenter), BorderLayout.CENTER);
+
+		add(PanelShop, BorderLayout.CENTER);
 		
-		panelShop =  new PanelShop(controller);
-		add(new JScrollPane(panelActualCenter), BorderLayout.CENTER);
+		
+		JPanel panelButtonsOptions = new JPanel();
+		panelButtonsOptions.setLayout(new GridLayout(1,4));
+
+		btnFavorites = new JButton();
+		btnFavorites.addActionListener(controller);
+		btnFavorites.setBorderPainted(false);
+		btnFavorites.setBackground(Color.WHITE);
+		btnFavorites.setIcon(new ImageIcon(new ImageIcon("src/img/Baker.png").getImage().getScaledInstance(40, -10, Image.SCALE_AREA_AVERAGING)));
+		btnFavorites.setActionCommand(Actions.VIEW_USER_FAVORITES.toString());
+		panelButtonsOptions.add(btnFavorites);
+
+		btnListProducts = new JButton();
+		btnListProducts.addActionListener(controller);
+		btnListProducts.setBorderPainted(false);
+		btnListProducts.setBackground(Color.WHITE);
+		btnListProducts.setIcon(new ImageIcon(new ImageIcon("src/img/Owner.png").getImage().getScaledInstance(40, -10, Image.SCALE_AREA_AVERAGING)));
+		btnListProducts.setActionCommand(Actions.OPT_USER_VIEW_PRODUCTS.toString());
+		panelButtonsOptions.add(btnListProducts);
+
+		btnShoppingCar = new JButton();
+		btnShoppingCar.addActionListener(controller);
+		btnShoppingCar.setBorderPainted(false);
+		btnShoppingCar.setBackground(Color.WHITE);
+		btnShoppingCar.setIcon(new ImageIcon(new ImageIcon("src/img/buyer.png").getImage().getScaledInstance(40, -10, Image.SCALE_AREA_AVERAGING)));
+		btnShoppingCar.setActionCommand(Actions.SHOPPING_CAR_USER.toString());
+		panelButtonsOptions.add(btnShoppingCar);
+		
+		btnSettings = new JButton();
+		btnSettings.addActionListener(controller);
+		btnSettings.setBorderPainted(false);
+		btnSettings.setBackground(Color.WHITE);
+		btnSettings.setIcon(new ImageIcon(new ImageIcon("src/img/Menu.png").getImage().getScaledInstance(40, -10, Image.SCALE_AREA_AVERAGING)));
+		btnSettings.setActionCommand(Actions.USER_SETTINGS.toString());
+		panelButtonsOptions.add(btnSettings);
+
+		
+		
+		panelButtonsOptions.setOpaque(true);
+		
+		add(panelButtonsOptions, BorderLayout.SOUTH);
+
 	}
 	
-	public void refreshShopList(ArrayList<Shop> shops , Controller controller){
-		panelActualCenter.removeAll();
-		panelShop.refreshCardRestaurant(shops);
-		panelActualCenter.add(panelShop);
-		panelActualCenter.updateUI();
-		panelActualCenter.repaint();
-	}
 
 
 	public void changeToProductsFromShopPanel(ArrayList<Product> productsFromShop) {
@@ -53,4 +106,5 @@ public class MainWindowUser extends JFrame {
 		panelActualCenter.updateUI();
 		panelActualCenter.repaint();
 	}
+
 }
