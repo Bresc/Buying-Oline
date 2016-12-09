b4b5e5d8f4133a3fcf24c6f2ef10222318b10da4
Angye <angi-danitv@hotmail.com>
2016-12-09 18:08:03 -0500

    Colores en Dialogo Add Shop

--- Buying Online/src/view/admin/ConstantUIAdmin.java
+++ Buying Online/src/view/admin/ConstantUIAdmin.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 import javax.swing.ImageIcon;
 
 public class ConstantUIAdmin {
-	public static final Color BTN_COLOR = Color.decode("#4d90fe");
+	public static final Color BTN_COLOR = Color.decode("#9fbb58");
 	public static final Color BACKGROUND_COLOR = Color.decode("#00BBBB");
 	public static final ImageIcon ICON_MAIN_WINDOW_ADMIN = new ImageIcon("src/img/administrator-icon.png");
 }
